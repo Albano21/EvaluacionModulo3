b32aa89d73ee1d4446228b6b6ea85ec00bfa9130
Albano Zupichiatti <89983599+Albano21@users.noreply.github.com>
2022-05-18 09:02:59 -0300

    Initial commit

---
+++ src/main/java/com/tppasantia/abmlibros/AbmlibrosApplication.java
@@ -0,0 +1,13 @@
+package com.tppasantia.abmlibros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbmlibrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbmlibrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tppasantia/abmlibros/AbmlibrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tppasantia.abmlibros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbmlibrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
